0dda18b2e58ac2eb673d9cf51ef6049f03c6ee5c
anxietypb <anxiety.pb@gmail.com>
2019-10-18 00:49:33 +0200

    feat: added array access AST output

--- test/visitor.test.js
+++ test/visitor.test.js
@@ -90,6 +90,15 @@ describe('JassVisitor', () => {
     expect(parse(input)).toMatchSnapshot()
   })
 
+  it('correctly constructs AST for identifier array access with left hand array access', () => {
+    const input = `
+    function test takes nothing returns nothing
+      set test[50] = someother[CreateUnit()][5]
+    endfunction      
+    `
+    expect(parse(input)).toMatchSnapshot()
+  })
+
   it('correctly constructs AST for complex jass input', () => {
     const input = fs.readFileSync(path.resolve(__dirname, './samples/war3map.j'), 'utf8')
     expect(parse(input)).toMatchSnapshot()
